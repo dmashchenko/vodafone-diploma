dc0f03160d5bca797b08e966fd81d2bdd62369f3
dmashchenko <mashchenkod@gmail.com>
2021-01-22 16:11:25 +0200

    project setup

---
+++ ml/src/config/constants.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+import configparser
+
+config = configparser.ConfigParser()
+abspath_of_this_script = os.path.abspath(os.path.realpath(__file__))
+
+config.read(os.path.split(abspath_of_this_script)[0] + '/../../../config.cfg')
+
+
+PROJECT_ROOT = Path(config['DEFAULT']['PROJECT_ROOT'])
+RAW_DATA_DIR = Path(config['DEFAULT']['RAW_DATA_DIR'])
