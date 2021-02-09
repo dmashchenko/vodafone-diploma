1d94b71145ca5f042ad54645ab8d77918ebe3f1d
dmashchenko <mashchenkod@gmail.com>
2021-02-09 17:44:04 +0200

    dataset refactoring

--- ml/src/data/dataset.py
+++ ml/src/data/dataset.py
@@ -1,6 +1,3 @@
-from dataclasses import dataclass, field
-from typing import List
-
 import pandas as pd
 
 from features.extraction import add_traff_features, statistic_columns
@@ -10,8 +7,7 @@ from config.constants import PROJECT_ROOT, PREPROCESSED_DATA_DIR
 INDEX_COLUMN = 'abon_id'
 
 
-@dataclass(frozen=True)
 class DataSet:
-    testdf: pd.DataFrame = add_traff_features(pd.read_parquet(PREPROCESSED_DATA_DIR / 'testdf.dump'))
-    traindf: pd.DataFrame = add_traff_features(pd.read_parquet(PREPROCESSED_DATA_DIR / 'traindf.dump'))
-    columns: List = traindf.columns.values
+    testdf = add_traff_features(pd.read_parquet(PREPROCESSED_DATA_DIR / 'testdf.dump'))
+    traindf = add_traff_features(pd.read_parquet(PREPROCESSED_DATA_DIR / 'traindf.dump'))
+    columns = traindf.columns.values
