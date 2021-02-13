e4d83b00ad669ee7300f2bc59ad15f9aa130380e
dmashchenko <mashchenkod@gmail.com>
2021-02-13 21:45:49 +0200

    config refactoring RESULT_DIR constant

--- ml/src/config/constants.py
+++ ml/src/config/constants.py
@@ -5,10 +5,12 @@ import configparser
 config = configparser.ConfigParser()
 abspath_of_this_script = os.path.abspath(os.path.realpath(__file__))
 
-config.read(os.path.split(abspath_of_this_script)[0] + '/../../../config.cfg')
+PROJECT_ROOT = Path(os.path.split(abspath_of_this_script)[0] + '/../../..').resolve()
 
+config.read(PROJECT_ROOT / 'config.cfg')
 
-PROJECT_ROOT = Path(config['DEFAULT']['PROJECT_ROOT'])
 RAW_DATA_DIR = Path(config['DEFAULT']['RAW_DATA_DIR'])
 
 PREPROCESSED_DATA_DIR = PROJECT_ROOT / 'ml/data/preprocessed'
+
+RESULT_DIR = PROJECT_ROOT / 'result'
