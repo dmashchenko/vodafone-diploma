8d73f4c0f744918904c888afa1ee46e25dad718d
dmashchenko <mashchenkod@gmail.com>
2021-02-13 22:22:17 +0200

    1. TrafficPredictor 2. Pickle serializer

---
+++ ml/src/model/production.py
@@ -0,0 +1,29 @@
+import pickle
+from pathlib import Path
+
+from config.constants import RESULT_DIR
+
+
+class TrafficPredictor:
+    def __init__(self, model=None, raw_features=[], features_nan_fillers={}):
+        self.model = model
+        self.raw_features = raw_features
+
+    def predict(self, X):
+        X = X[self.raw_features]
+        # impute
+        # extract features
+        return self.model.predict(self.prepare(X))
+
+    @staticmethod
+    def prepare(X):
+        pass
+
+
+def build_traffic_predictor():
+    return TrafficPredictor()
+
+
+def publish(predictor: TrafficPredictor):
+    Path(RESULT_DIR).mkdir(exist_ok=True)
+    pickle.dump(predictor, open(RESULT_DIR / 'traffic_predictor.pickle', 'wb'))
