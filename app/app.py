87d47ff78023a603627377b6c39188cb6f16eff7
dmashchenko <mashchenkod@gmail.com>
2021-02-19 10:52:47 +0200

    app ukraine bubble map

--- app/app.py
+++ app/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+def root():
+    return render_template('index.html')
+
+@app.route('/ukraine.geo.json')
+def json():
+    return app.send_static_file('ukraine.geo.json')
 
 
 if __name__ == '__main__':
