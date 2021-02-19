c03ec5475e053d245c9f8438081407ad362b1d53
dmashchenko <mashchenkod@gmail.com>
2021-02-19 11:59:34 +0200

    app: get markers from flask

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import json
 
 app = Flask(__name__)
 
@@ -8,8 +9,17 @@ app = Flask(__name__)
 def root():
     return render_template('index.html')
 
+
+@app.route('/data')
+def data():
+    d = [{'long': 30.083, 'lat': 50.149, 'group': "A", 'size': 60},
+         {'long': 31.26, 'lat': 47.71, 'group': "C", 'size': 20},
+         {'long': 29.349, 'lat': 48.864, 'group': "B", 'size': 87}]
+    return json.dumps(d)
+
+
 @app.route('/ukraine.geo.json')
-def json():
+def map():
     return app.send_static_file('ukraine.geo.json')
 
 
