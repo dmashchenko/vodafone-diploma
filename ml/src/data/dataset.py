c323282d97784afb8ff32ded947a33ddaef660d7
dmashchenko <mashchenkod@gmail.com>
2021-03-06 17:08:30 +0200

    add geo features and dataset

--- ml/src/data/dataset.py
+++ ml/src/data/dataset.py
@@ -1,15 +1,37 @@
 import pandas as pd
+import numpy as np
 
-from features.extraction import add_traff_features, statistic_columns
+from features.extraction import add_traff_features, statistic_columns, add_real_lon_lat, add_city_feature
 
-from config.constants import PROJECT_ROOT, PREPROCESSED_DATA_DIR
+from config.constants import PROJECT_ROOT, PREPROCESSED_DATA_DIR, RAW_DATA_DIR
 
 INDEX_COLUMN = 'abon_id'
 
 
+def add_extra_features(df, geodf):
+    df = add_traff_features(df)
+    df = add_real_lon_lat(df, geodf)
+    return add_city_feature(df, geodf)
+
+
+def prepare_geodf(result):
+    result.lng = result.lng.astype(np.float32)
+    result.lat = result.lat.astype(np.float32)
+    result.population = result.population.fillna(result.population.min() / 2.0).astype(np.float32)
+    result.capital = result.capital.fillna('minor')
+
+    result = result.set_index('city')
+    result.rename(columns={'lng': 'loc_lon', 'lat': 'loc_lat'}, inplace=True)
+    result.drop(result[result.index.duplicated(keep='first')].index, inplace=True)
+
+    return result
+
+
 class DataSet:
-    testdf = add_traff_features(pd.read_parquet(PREPROCESSED_DATA_DIR / 'testdf.dump'))
-    traindf = add_traff_features(pd.read_parquet(PREPROCESSED_DATA_DIR / 'traindf.dump'))
+    geodf = prepare_geodf(pd.read_csv(RAW_DATA_DIR / 'extra/ua.csv',
+                                      usecols=['lng', 'lat', 'city', 'population', 'capital']))
+    testdf = add_extra_features(pd.read_parquet(PREPROCESSED_DATA_DIR / 'testdf.dump'), geodf)
+    traindf = add_extra_features(pd.read_parquet(PREPROCESSED_DATA_DIR / 'traindf.dump'), geodf)
     columns = traindf.columns.values
 
 
@@ -22,11 +44,11 @@ def get_traffic_by_station_df(df):
 def get_traffic_by_station_for_animation_df(stationdf):
     result = pd.DataFrame()
 
-    for i in range(1, 6, 1):
+    for i in range(5, 0, -1):
         col_name = f'traff_m{i}'
         traffdf = stationdf.loc[:, ('loc_lat', 'loc_lon', col_name)]
         traffdf.rename(columns={col_name: 'traffic'}, inplace=True)
-        traffdf['month'] = i
+        traffdf['month'] = 6 - i
         result = result.append(traffdf, ignore_index=True)
 
     targetdf = stationdf.loc[:, ('loc_lat', 'loc_lon', 'target')]
--- ml/src/features/extraction.py
+++ ml/src/features/extraction.py
@@ -1,4 +1,7 @@
+import sys
 from sklearn.cluster import KMeans
+import numpy as np
+import pandas as pd
 
 STATISTICS_POSTFIXES = ['mea_mnt', 'max_mnt', 'min_mnt', 'std_mnt', 'td_mnt', 'mea_wk']
 
@@ -30,4 +33,50 @@ def add_traff_features(df):
     df['traff_min'] = traff_df.min(axis=1)
     df['traff_max'] = traff_df.max(axis=1)
     df['traff_std'] = traff_df.std(axis=1)
+    return df
+
+
+def add_real_lon_lat(df, geodf):
+    guessed_geodf = pd.DataFrame(data={'city': ['Odesa', 'Chernivtsi', 'Kyiv', 'Kharkiv', 'Sumy', 'Dnipro', 'Mykolaiv'],
+                                       'loc_lon': [11.75, 10.6, 11.75, 12.85, 12.5, 12.55, 12.05],
+                                       'loc_lat': [14.74, 15.04, 15.36, 15.32, 15.49, 15.08, 14.85]}).set_index('city')
+
+    geo = pd.merge(guessed_geodf, geodf, left_index=True, right_index=True)
+
+    lon_coeff = linear_regression(geo.loc_lon_x, geo.loc_lon_y)
+    lat_coeff = linear_regression(geo.loc_lat_x, geo.loc_lat_y)
+
+    df['loc_lon'] = df['loc_lon'].apply(lambda x: np.round(lon_coeff[0] + x * lon_coeff[1], decimals=4))
+    df['loc_lat'] = df['loc_lat'].apply(lambda x: np.round(lat_coeff[0] + x * lat_coeff[1], decimals=4))
+    return df
+
+
+def linear_regression(X, y):
+    X = np.asarray(list(zip([1] * len(X), X)))
+    X_transp = np.transpose(X)
+    inverted_matrix = np.linalg.inv(np.dot(X_transp, X))
+    return np.dot(inverted_matrix, np.dot(X_transp, np.transpose(y)))
+
+
+def get_city_by_geo(lat, lon, geo_df):
+    point = np.array((lat, lon))
+    min_distance = [sys.float_info.max, 'City']
+    for index, row in geo_df.iterrows():
+        d = np.linalg.norm(np.array((row['loc_lat'], row['loc_lon'])) - point)
+        if min_distance[0] > d:
+            min_distance[0] = d
+            min_distance[1] = index
+    return min_distance[1]
+
+
+def add_city_feature(df, geodf):
+    geo_city_dict = {}
+
+    for index, row in df.groupby(['loc_lon', 'loc_lat']).size().reset_index().iterrows():
+        lat_ = row['loc_lat']
+        lon_ = row['loc_lon']
+        geo_city_dict[(lat_, lon_)] = get_city_by_geo(lat_, lon_, geodf)
+
+    df['city'] = df[['loc_lat', 'loc_lon']].dropna().apply(lambda x: geo_city_dict[(x.loc_lat, x.loc_lon)], axis=1)
+    df.city.fillna('NoGeo', inplace=True)
     return df
