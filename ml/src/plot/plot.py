96c46a12dd7843e1256f885e123ee86ffe098312
dmashchenko <mashchenkod@gmail.com>
2021-03-08 15:25:01 +0200

    plot parallel coords

--- ml/src/plot/plot.py
+++ ml/src/plot/plot.py
@@ -32,3 +32,29 @@ def _get_col_index_list(cols, rows):
     for _ in range(rows):
         col_index += list_to_repeat
     return col_index
+
+
+def traffic_parallel_coords(df, max_value=50000, range=[0.8, 1]):
+    df = df[df.target < max_value]
+    dimensions = []
+
+    for col in ['traff_m5', 'traff_m4', 'traff_m3', 'traff_m2', 'traff_m1', 'target']:
+        dimensions.append(dict(range=[0, max_value],
+                               tickvals=[0, 20, max_value * 0.5, 120, max_value],
+                               label=col, values=df[col]))
+        if col == 'target':
+            dimensions[-1]['constraintrange'] = [max_value * range[0], max_value * range[1]]
+
+    fig = go.Figure(data=
+    go.Parcoords(
+        line=dict(color=df['target'],
+                  colorscale=px.colors.sequential.Rainbow,
+                  showscale=True,
+                  cmin=0,
+                  cmax=max_value),
+        dimensions=list(dimensions)
+    )
+    )
+    # fig.update_layout(template='plotly_dark', paper_bgcolor='rgba(0,0,0,0)',
+    #                   plot_bgcolor='rgba(0,0,0,0)', font=dict(color="white", size=18, ), width=1100, height=600)
+    return fig
